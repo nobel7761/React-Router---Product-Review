11dfd4ef988a83990286e9a96211788281e938b4
nobel7761 <habiburnobel@gmail.com>
2022-04-04 23:54:11 +0600

    navbar and routes completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Header></Header>
       <h1>Project Ready</h1>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <div className='navbar'>
-            {/* <nav className='nav'>
-                <Link to='/'>HOME</Link>
-                <Link to='/reviews'>REVIEWS</Link>
-                <Link to='/dashboard'>DASHBOARD</Link>
-                <Link to='/blogs'>BLOGS</Link>
-                <Link to='/about'>ABOUT</Link>
-                <Link to='/hello'>hello</Link>
-            </nav> */}
-        </div>
+        <div className='header'>
+            <Navbar expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <CustomLink to="/" className='nav-link'>HOME</CustomLink>
+                        <CustomLink to="/reviews" className='nav-link'>REVIEWS</CustomLink>
+                        <CustomLink to="/dashboard" className='nav-link'>DASHBOARD</CustomLink>
+                        <CustomLink to="/blogs" className='nav-link'>BLOGS</CustomLink>
+                        <CustomLink to="/gallery" className='nav-link'>GALLERY</CustomLink>
+                        <CustomLink to="/about" className='nav-link'>ABOUT</CustomLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div >
+
     );
 };
 
