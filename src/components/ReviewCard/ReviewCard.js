502270eeee732ec04f544df864a806c554675f35
nobel7761 <habiburnobel@gmail.com>
2022-04-05 14:42:21 +0600

    rating icon integrated

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import ReactStars from "react-rating-stars-component";
 import './ReviewCard.css';
 
 
 const ReviewCard = (props) => {
     const { name, picture, review, rating } = props.review;
+
+
+
+
     return (
         <div className='card-container'>
             <Image className='card-image' src={picture} />
-            <div className='review-details'>
-                <h3 className='fw-bold'>{name}</h3>
+            <div className='review-details position-relative'>
+                <div className='position-absolute top-0 mt-3'>
+                    <h3 className='fw-bold '>{name}</h3>
+                </div>
                 <p title={review}>
                     {review.length > 200 ? review.slice(0, 200) + '...' : review}
                 </p>
-                <p>Ratings: <small>{rating} Star</small></p>
+                <p className='d-flex align-items-center position-absolute bottom-0'>Ratings: <ReactStars
+                    count={5}
+                    value={rating}
+                    size={28}
+                    isHalf={true}
+                    color="gray"
+                    emptyIcon={<i className="far fa-star"></i>}
+                    halfIcon={<i className="fa fa-star-half-alt"></i>}
+                    fullIcon={<i className="fa fa-star"></i>}
+                    activeColor="blue"
+                    border="2px solid black"
+                /></p>
             </div>
         </div >
     );
